d34649bc56d5f98a67f7e1e2388fd6d0982955be
Yanxin Li <31481788+xinerli@users.noreply.github.com>
2023-04-25 20:29:13 -0500

    Add descriptorDWT

---
+++ code/shapeDTW_translate/descriptorDWT.R
@@ -0,0 +1,55 @@
+descriptorDWT <- function(subsequence, param) {
+  
+  val_param <- validateDWTdescriptorparam(param)
+  subsequence <- as.vector(subsequence)
+  seqlen <- length(subsequence)
+  
+  # Now only support subsequences with length less than 1024
+  refLens <- 2^(1:10)
+  gaps <- abs(refLens - seqlen)
+  idx <- which.min(gaps)
+  newlen <- 2^idx
+  
+  if (newlen < seqlen) {
+    subsequence <- subsequence[1:newlen]
+  } 
+  else {
+    subsequence <- c(subsequence, mean(subsequence) * rep(1, newlen - seqlen))
+  }
+  
+  selection <- val_param$selection
+  numLevels <- val_param$numLevels
+  n <- val_param$n
+  
+  listCoef <- wavecoef(selection, n)
+  h <- listCoef[[1]]
+  g <- listCoef[[2]]
+  
+  cntLevels <- 0
+  w <- NULL
+  s <- subsequence
+  
+  while (length(s) >= 4 && cntLevels < numLevels) {
+    listCoef <- fwt1step(s, h, g)
+    sout <- listCoef[[1]]
+    dout <- listCoef[[2]]
+    w <- rbind(dout, w)
+    s <- sout
+    cntLevels <- cntLevels + 1
+  }
+  
+  if (cntLevels == numLevels) {
+    w <- rbind(sout, w)
+    rep <- t(w)
+    return(rep)
+  } 
+  else if (cntLevels < numLevels && length(s) < 4) {
+    listCoef <- fwt1step(s, h, g)
+    sout <- listCoef[[1]]
+    dout <- listCoef[[2]]
+    w <- rbind(c(sout, dout), w)
+    rep <- t(w)
+    return(rep)
+  }
+ 
+}
