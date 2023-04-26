9669ab781f6d65be56a5aabb4e2c40a9d9e4c62c
Yanxin Li <31481788+xinerli@users.noreply.github.com>
2023-04-25 19:46:39 -0500

    Add files via upload

--- code/shapeDTW_translate/validateHOG1Dparam.R
+++ code/shapeDTW_translate/validateHOG1Dparam.R
@@ -1,50 +1,52 @@
 validateHOG1Dparam <- function(param = NULL) {
+  
   # a typical valid setting
-  # {
-  #   param.blocks    = [1 2];  # unit: cell
-  #   param.cells     = [1 25]; # unit: t
-  #   param.gradmethod = 'centered'; # 'centered'
-  #   param.nbins = 8;
-  #   param.sign = 'true';
-  # }
-  if (is.null(param) || !is.list(param)) {
-    val_param <- list(
-      # blocks = c(1, 2),
-      cells = c(1, 25),
-      overlap = 0,
-      gradmethod = "centered",
-      nbins = 8,
-      sign = "true",
-      xscale = 0.1
-      # cutoff = c(2, 51)
-    )
+  # param.blocks <- c(1, 2) # unit: cell
+  param.cells <- c(1, 25) # unit: t
+  val_param <- list(cells = param.cells, overlap = 0, gradmethod = 'centered', nbins = 8, sign = 'true', xscale = 0.1)
+  
+  if (is.null(param)) {
     return(val_param)
   }
   
-  val_param <- param
+  if (!is.list(param)) {
+    return(val_param)
+  }
   
-  if (!("overlap" %in% names(val_param))) {
+  if (!'overlap' %in% names(param)) {
     val_param$overlap <- 12
+  } else {
+    val_param$overlap <- param$overlap
   }
   
-  if (!("xscale" %in% names(val_param))) {
+  if (!'xscale' %in% names(param)) {
     val_param$xscale <- 0.1
+  } else {
+    val_param$xscale <- param$xscale
   }
   
-  if (!("cells" %in% names(val_param))) {
+  if (!'cells' %in% names(param)) {
     val_param$cells <- c(1, 25)
+  } else {
+    val_param$cells <- param$cells
   }
   
-  if (!("gradmethod" %in% names(val_param))) {
-    val_param$gradmethod <- "uncentered"
+  if (!'gradmethod' %in% names(param)) {
+    val_param$gradmethod <- 'uncentered'
+  } else {
+    val_param$gradmethod <- param$gradmethod
   }
   
-  if (!("nbins" %in% names(val_param))) {
+  if (!'nbins' %in% names(param)) {
     val_param$nbins <- 8
+  } else {
+    val_param$nbins <- param$nbins
   }
   
-  if (!("sign" %in% names(val_param))) {
-    val_param$sign <- "true"
+  if (!'sign' %in% names(param)) {
+    val_param$sign <- 'true'
+  } else {
+    val_param$sign <- param$sign
   }
   
   return(val_param)
