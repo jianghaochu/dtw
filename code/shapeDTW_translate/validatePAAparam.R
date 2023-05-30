797962ed0c1f9160fc6c51fdb172e1808875b080
Yanxin Li <31481788+xinerli@users.noreply.github.com>
2023-05-29 19:27:44 -0500

    Update validatePAAparam.R

--- code/shapeDTW_translate/validatePAAparam.R
+++ code/shapeDTW_translate/validatePAAparam.R
@@ -1,28 +1,26 @@
-# validate parameters of piecewise aggregate approximation
 validatePAAparam <- function(param) {
-  
   if (missing(param) || !is.list(param)) {
-    val_param <- list('segNum' = 10, 'segLen' = NULL, 'priority' = "segNum")
+    val_param <- list(segNum = 10, segLen = NULL, priority = "segNum")
     return(val_param)
   }
   
   val_param <- list()
   
   if (!is.null(param$segNum)) {
-    if (param$segNum > 0) {
-      val_param$segNum <- param$segNum
-    } else {
+    if (param$segNum <= 0) {
       val_param$segNum <- 10
+    } else {
+      val_param$segNum <- param$segNum
     }
   } else {
     val_param$segNum <- 10
   }
   
   if (!is.null(param$segLen)) {
-    if (param$segLen > 0) {
-      val_param$segLen <- param$segLen
-    } else {
+    if (param$segLen <= 0) {
       val_param$segLen <- 5
+    } else {
+      val_param$segLen <- param$segLen
     }
   } else {
     val_param$segLen <- 5
@@ -31,22 +29,22 @@ validatePAAparam <- function(param) {
   if (!is.null(param$priority)) {
     val_param$priority <- param$priority
   } else {
-    val_param$priority <- 'segNum'
+    val_param$priority <- "segNum"
   }
   
   switch(val_param$priority,
-         'segNum' = {
+         "segNum" = {
            if (is.null(val_param$segNum) || val_param$segNum <= 0) {
              val_param$segNum <- 10
            }
          },
-         'segLen' = {
+         "segLen" = {
            if (is.null(val_param$segLen) || val_param$segLen <= 0) {
              val_param$segLen <- 5
            }
          },
          {
-           val_param$priority <- 'segNum'
+           val_param$priority <- "segNum"
            if (is.null(val_param$segNum) || val_param$segNum <= 0) {
              val_param$segNum <- 10
            }
@@ -54,4 +52,7 @@ validatePAAparam <- function(param) {
   )
   
   return(val_param)
+  
 }
+
+#validatePAAparam()
