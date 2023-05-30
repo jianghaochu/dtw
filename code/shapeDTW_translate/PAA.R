80cd2059fed05b468ded5836a4a9414de2f840f1
Yanxin Li <31481788+xinerli@users.noreply.github.com>
2023-05-29 19:29:59 -0500

    Update PAA.R

--- code/shapeDTW_translate/PAA.R
+++ code/shapeDTW_translate/PAA.R
@@ -1,9 +1,10 @@
-PAA <- function(subsequence, param) {
+PAA <- function(sequences, param) {
+
   val_param <- validatePAAparam(param)
-  len <- length(subsequence)
+  len <- length(sequences)
   
   switch(val_param$priority,
-         'segNum' = {
+         "segNum" = {
            segNum <- val_param$segNum
            segLen <- floor(len / segNum)
            
@@ -12,7 +13,7 @@ PAA <- function(subsequence, param) {
              segLen <- len
            }
          },
-         'segLen' = {
+         "segLen" = {
            segLen <- val_param$segLen
            segNum <- floor(len / segLen)
            
@@ -23,21 +24,24 @@ PAA <- function(subsequence, param) {
          }
   )
   
-  if (segNum > length(subsequence)) {
-    stop('Inconsistency found between parameter setting and input sequences\n')
+  if (segNum > length(sequences)) {
+    stop("Inconsistency found between parameter setting and input sequences\n")
   }
   
+  # Redefine a new list of validated parameters that are used in PAA
   val_params <- list(
-    'priority' = val_param$priority,
-    'segNum' = segNum,
-    'segLen' = segLen
+    "priority" = val_param$priority,
+    "segNum" = segNum,
+    "segLen" = segLen
   )
   
+  # If segment length * segment number < length of the sequences, add the remaining to last segment
   if (segNum * segLen < len) {
     segLens <- c(rep(segLen, segNum - 1), len - segLen * (segNum - 1))
   } else if (segNum * segLen == len) {
     segLens <- rep(segLen, segNum)
   } else {
+    # If segment length * segment number > length of the sequences, remove extra from the segment
     segLens <- c(rep(segLen, segNum - 1), segLen - (segLen * segNum - len))
   }
   
@@ -45,15 +49,13 @@ PAA <- function(subsequence, param) {
   
   segs <- numeric(segNum)
   for (i in 1:segNum) {
-    segs[i] <- mean(subsequence[(idx_seg[i] + 1):idx_seg[i + 1]])
+    segs[i] <- mean(sequences[(idx_seg[i] + 1):idx_seg[i + 1]])
   }
   
-  out <- list(
-    'rep' = segs,
-    'segLens' = segLens,
-    'idx_seg' = idx_seg,
-    'val_params' = val_params
-  )
-  
+  out <- list("descriptor" = segs,
+              "segLens" = segLens,
+              "idx_seg" = idx_seg,
+              "val_params" = val_params)
   return(out)
+
 }
